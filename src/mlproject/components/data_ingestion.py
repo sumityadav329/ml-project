d4c7cafb34ed2b77a4aeac6a88ccb3dcbd6aa183
Sumit Yadav <sumityadav329@gmail.com>
2024-03-28 12:20:46 +0530

    Data Ingestion

--- src/mlproject/components/data_ingestion.py
+++ src/mlproject/components/data_ingestion.py
@@ -14,9 +14,9 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path:str = os.path.join('artifact', 'train.csv')
-    test_data_path:str = os.path.join('artifact', 'test.csv')
-    raw_data_path:str = os.path.join('artifact', 'raw.csv')
+    train_data_path:str = os.path.join('artifacts', 'train.csv')
+    test_data_path:str = os.path.join('artifacts', 'test.csv')
+    raw_data_path:str = os.path.join('artifacts', 'raw.csv')
 
 
 class DataIngestion:
